dbd2ef6f6a670905e6b75a912f3a3030512e084e
YodaGroguWhite <ddkokalj@gmail.com>
2026-02-17 22:01:00 +0300

    Update flights.js

--- api/flights.js
+++ api/flights.js
@@ -1,14 +1,19 @@
 export default async function handler(req, res) {
-  const { origin, destination, departure_at } = req.query;
+  const { origin, destination, departure_at, days = 7 } = req.query;
 
-  const url = `https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=${origin}&destination=${destination}&departure_at=${departure_at}`;
+  const url = `https://api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=${origin}&destination=${destination}&departure_at=${departure_at}&limit=${days}&sorting=price`;
 
-  const response = await fetch(url, {
-    headers: {
-      "X-Access-Token": process.env.TP_TOKEN
-    }
-  });
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "X-Access-Token": process.env.TP_TOKEN
+      }
+    });
 
-  const data = await response.json();
-  res.status(200).json(data);
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: "Server error", details: err.message });
+  }
 }
